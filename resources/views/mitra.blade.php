5207691e5efd2cbb0bf17a687266ddfcd4edb2e0
alfito24 <khansaalfito@gmail.com>
2022-04-21 23:59:41 +0700

    update-page-mitra

--- resources/views/mitra.blade.php
+++ resources/views/mitra.blade.php
@@ -1,128 +1,7 @@
 @extends("layouts.master")
 @section('content')
 
-    {{-- <div class="container">
-        <h3 class="text-center">Mitra Oasis Oleh-oleh</h3>
-        <div class="row row-cols-1 row-cols-md-3 g-3">
-            <div class="col">
-                @foreach ($mitra as $m)
-                    <div class="card mt-3">
-                        <img src="{{ $m->foto_mitra }}" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $m->namamitra }}</h5>
-                            <p class="card-text"><i class="bi bi-geo-alt"></i>{{ $m->alamat_mitra }}</p>
-                            <h6 class="card-text">{{ $m->kota }}-{{ $m->provinsi }}</h6>
-                            <a href="#" class="btn btn-outline-primary" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal">Lihat Detail</a>
-                        </div>
-                    </div>
 
-                    <!-- Modal -->
-                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                        aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-scrollable modal-xl modal-fullscreen-lg-down">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">{{ $m->namamitra }}</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <h5 class="mx-3">Detail {{ $m->namamitra }}</h5>
-                                    <div class="px-3">
-                                        <div class="row">
-                                            <div class="col-7">
-                                                <figure class="figure">
-                                                    <img class="d-block ratio ratio-16x9 rounded"
-                                                        src="{{ $m->foto_mitra }}" style="object-fit: cover;">
-                                                    <figcaption class="figure-caption text-center">Foto Lokasi
-                                                        {{ $m->namamitra }}</figcaption>
-                                                </figure>
-                                            </div>
-                                            <div class="col">
-                                                <h6>Tentang {{ $m->namamitra }}</h6>
-                                                <p>{{ $m->deskripsimitra }}</p>
-                                                <div class="row">
-                                                    <div class="col-12">
-                                                        <table class="table table-borderless">
-                                                            <tbody>
-                                                                <tr>
-                                                                    <td class="col-4 bi bi-dot">Kontak</td>
-                                                                    <td class="col-1 text-center">:</td>
-                                                                    <td class="col-7"><i class="bi bi-whatsapp"></i> {{ $m->kontak }}</td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <td class="col-4 bi bi-dot">Alamat</td>
-                                                                    <td class="col-1 text-center">:</td>
-                                                                    <td class="col-7">{{ $m->alamat_mitra }}
-                                                                    </td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <td class="col-4 bi bi-dot">Jam Buka</td>
-                                                                    <td class="col-1 text-center">:</td>
-                                                                    <td class="col-7">{{ $m->jam_buka_mitra }}
-                                                                    </td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <td class="col-4 bi bi-dot">Sampel Cicip</td>
-                                                                    <td class="col-1 text-center">:</td>
-                                                                    <td class="col-7">{{ $m->cicip }}</td>
-                                                                </tr>
-                                                            </tbody>
-                                                        </table>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <h5>Produk {{ $m->namamitra }}</h5>
-
-                                        <div class="row row-cols-1 row-cols-md-4 g-4 mb-3">
-                                            @foreach ($produkmitra as $pm)
-                                                @if ($pm->idmitra = $m->idmitra)
-                                                    <div class="col">
-                                                        <div class="card h-100">
-                                                            <img class="d-block ratio ratio-16x9 rounded"
-                                                                src="{{ $pm->gambar_produk_mitra }}"
-                                                                style="object-fit: cover;">
-                                                            <div class="card-body">
-                                                                <h5 class="card-title">{{ $pm->nama_produk_mitra }}
-                                                                </h5>
-                                                                <p class="card-text">Harga Produk :
-                                                                    {{ $pm->harga_produk_mitra }} <br>
-                                                                    @if ($pm->status_produkmitra == 1)
-                                                                        Produk Asli Dapur {{ $pm->namamitra }}
-                                                                    @else
-                                                                        Produk Partner {{ $pm->namamitra }}
-                                                                    @endif
-                                                                </p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                @endif
-                                            @endforeach
-                                        </div>
-
-                                        <h5>Lokasi {{ $m->namamitra }}</h5>
-                                        <div class="rounded">
-                                            @php
-                                                $embed = $m->gmap_mitra;
-                                                $peta = stripslashes($embed);
-                                                echo $embed;
-                                            @endphp
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <div class="modal-footer">
-                                    <a type="button" class="btn btn-outline-primary" href="{{$m->link}}" target="blank">Hubungi</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    </div> --}}
     <style>
         .carousel-item{
             height: 32rem;
@@ -166,11 +45,11 @@
     <div class="container">
         <div class="row">
             <div class="col-5 mt-3">
-                <p style="font-size: 2rem">Kamu sedang menelusuri toko, </p>
+                <p style="font-size: 2rem">Kamu sedang menelusuri mitra, </p>
 
 
 
-                <h1>{{ $toko->nama_toko }}</h1>
+                <h1>{{ $toko->nama_mitra }}</h1>
 
                 <h4>#1 Toko Oleh-Oleh Surabaya</h4>
                 <p><i>Menurut <b> <span style="color: orange">100</span> </b> Orang Surabaya</i></p>
@@ -197,7 +76,7 @@
     <div class="container">
         <div class="row mt-4">
             <div class="col-8">
-                <h1 class="">Produk Oleh-oleh dari {{ $toko->nama_toko }}</h1>
+                <h1 class="">Produk Oleh-oleh dari {{ $toko->nama_mitra }}</h1>
             </div>
             <div class="col-2"></div>
             <div class="col-2" >
@@ -205,15 +84,15 @@
             </div>
         </div>
     </div>
-    @foreach ($produk_toko as $p )
+    @foreach ($produk_mitra as $p )
     <div class="container ">
         <div class="row mt-3">
             <div class="col-4">
-                <img width="410px" src="https://images.pexels.com/photos/9938862/pexels-photo-9938862.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+                <img width="410px" src="{{ $p->gambar_produk_mitra }}" alt="">
             </div>
             <div class="col-5 mt-4">
-                <h1>{{ $p->nama_produk }}</h1>
-                <h3>Rp.{{ number_format($p->harga_produk) }}</h3>
+                <h1>{{ $p->nama_produk_mitra }}</h1>
+                <h3>Rp.{{ number_format($p->harga_produk_mitra) }}</h3>
                 <a href="" class="btn btn-secondary" style="font-size: 1.7rem"><i><span style="color: orange; font-weight:bold">#1</span> Spikoe di Toko ini</i></a>
             </div>
             <div class="col-3 mt-4">
@@ -256,8 +135,8 @@
         </div>
         <hr>
     </div> --}}
-    <div class="container">
-        <h1>Informasi {{ $toko->nama_toko }}</h1>
+    <div class="container mt-4">
+        <h1 >Informasi {{ $toko->nama_mitra }}</h1>
         <div class="row">
             <div class="col-6">
                 {{-- @php
@@ -285,7 +164,7 @@
                 <h3  class="mt-2">Fasilitas Toko</h3>
                 <div class="row" >
                 @php
-                    $fasil_toko = preg_split('/---/', $toko->fasilitas_toko);
+                    $fasil_toko = preg_split('/---/', $toko->fasilitas_mitra);
                 @endphp
                 @foreach ($fasil_toko as $f )
                 <div class="col-5" >
@@ -326,7 +205,7 @@
                 <h3  class="mt-2">Di Sekitar Toko</h3>
                 <div class="row" >
                 @php
-                    $sekita_toko = preg_split('/---/', $toko->jenis_sekitar);
+                    $sekita_toko = preg_split('/---/', $toko->sekitar_mitra);
                 @endphp
                 @foreach ($sekita_toko as $s )
                     @if ($loop->odd)
@@ -391,10 +270,10 @@
             <div class="row">
                 <div class="col-8">
                 @php
-                    $kontak_toko = preg_split('/---/', $toko->kontak_toko);
+                    $kontak_mitra = preg_split('/---/', $toko->kontak_mitra);
                 @endphp
                     <div class="row">
-                        @foreach ($kontak_toko as $k )
+                        @foreach ($kontak_mitra as $k )
                         <div class="col-6">
                             <i class='far fa-address-book'></i> <span href="www.spikoekuno.co.id"><i>{{ $k }}</i></span>
                         </div>
@@ -413,7 +292,7 @@
 
     </div>
     <div class="container carousel">
-        <h1 class="mt-3">Galeri {{ $toko->nama_toko }}</h1>
+        <h1 class="mt-3">Galeri {{ $toko->nama_mitra }}</h1>
         <div class="container" style="position: center">
         <div id="myCarousel" class="carousel" data-bs-ride="carousel">
             <div class="carousel-inner">
