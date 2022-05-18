9ba03720bb83abfa89aa32b090c7d2e1b3d89f66
Alfito24 <khansaalfito@gmail.com>
2022-05-18 17:02:01 +0700

    update-jam-operasional

--- app/Http/Controllers/TokoController.php
+++ app/Http/Controllers/TokoController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class TokoController extends Controller
 {
     public function show($id){
         $toko = DB::table('toko_oleh')->where('idtoko', $id)->first();
         $produk_toko = DB::table('produk_toko')->where('idtoko', $id)->get();
-        return view('toko', compact('toko', 'produk_toko'));
+        $now = Carbon::now();
+        $start = Carbon::createFromTimeString($toko->jam_buka);
+        $end = Carbon::createFromTimeString($toko->jam_tutup);
+        $check = $now->between($start, $end);
+        return view('toko', compact('toko', 'produk_toko', 'check'));
     }
 
 }
