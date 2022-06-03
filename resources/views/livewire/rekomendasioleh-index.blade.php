43c48eb684e4b1c86f797d49dcca08d231af6894
Daffa Daris Mahendra Ansori <5026201122@mahasiswa.integra.its.ac.id>
2022-06-03 11:57:27 +0700

    Update rekomendasioleh-index.blade.php

--- resources/views/livewire/rekomendasioleh-index.blade.php
+++ resources/views/livewire/rekomendasioleh-index.blade.php
@@ -26,15 +26,16 @@ $rekomendasiIdoleh;
                         class="button-oleh">Rekomendasikan <i class="bi bi-hand-thumbs-up"></i></a></label>
             @endif
         </div>
-        <div class="align-items-center">
-            <label  wire:click="rekomendasioleh({{ $idoleh }})"
-                class="bi bi-hand-thumbs-up-fill"><span style="color: red"> {{ $count }} </span>Orang</label>
-        </div>
+
     @endauth
     @guest
         <div class="align-items-center">
             <label><a href="/login" class=" button-oleh" style="">Rekomendasikan</a></label>
         </div>
     @endguest
+    <div class="align-items-center">
+        <label  wire:click="rekomendasioleh({{ $idoleh }})"
+            class="bi bi-hand-thumbs-up-fill"><span style="color: red"> {{ $count }} </span>Orang</label>
+    </div>
 </div>
 
