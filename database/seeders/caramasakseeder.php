27cbf76b4988301370483b93053e2c7f114ce482
Daffa Daris Mahendra Ansori <5026201122@mahasiswa.integra.its.ac.id>
2022-06-23 20:23:59 +0700

    fixing some seeder

--- database/seeders/caramasakseeder.php
+++ database/seeders/caramasakseeder.php
@@ -17,57 +17,57 @@ class caramasakseeder extends Seeder
         DB::table('masak')->insert([
             'idmasak'=>'1',
             'namamasak'=>'Dipanggang',
-            'gambamasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906200479996968960/Dipanggang.jpg',
+            'gambarmasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906200479996968960/Dipanggang.jpg',
         ]);
 DB::table('masak')->insert([
             'idmasak'=>'2',
             'namamasak'=>'Direbus',
-            'gambamasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906200719277821962/Direbus.jpeg',
+            'gambarmasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906200719277821962/Direbus.jpeg',
         ]);
 DB::table('masak')->insert([
             'idmasak'=>'3',
             'namamasak'=>'Dikukus',
-            'gambamasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906200537861603358/Dikukus.jpg',
+            'gambarmasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906200537861603358/Dikukus.jpg',
         ]);
 DB::table('masak')->insert([
             'idmasak'=>'4',
             'namamasak'=>'Digoreng',
-            'gambamasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906200860084805632/Digoreng.jpg',
+            'gambarmasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906200860084805632/Digoreng.jpg',
         ]);
 DB::table('masak')->insert([
             'idmasak'=>'5',
             'namamasak'=>'Disangrai',
-            'gambamasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906456515660115968/Disangrai.jpg',
+            'gambarmasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906456515660115968/Disangrai.jpg',
         ]);
 DB::table('masak')->insert([
             'idmasak'=>'6',
             'namamasak'=>'Direduksi',
-            'gambamasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906457320794497054/Direduksi.jpg',
+            'gambarmasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906457320794497054/Direduksi.jpg',
         ]);
 DB::table('masak')->insert([
             'idmasak'=>'7',
             'namamasak'=>'Difermentasi',
-            'gambamasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906200975033917450/Difermentasi.jpg',
+            'gambarmasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906200975033917450/Difermentasi.jpg',
         ]);
 DB::table('masak')->insert([
             'idmasak'=>'8',
             'namamasak'=>'Digiling',
-            'gambamasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906803300832268318/mengulek.jpg',
+            'gambarmasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/906803300832268318/mengulek.jpg',
         ]);
 DB::table('masak')->insert([
             'idmasak'=>'9',
             'namamasak'=>'Diperas',
-            'gambamasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/909673509507194910/diperas.jpg',
+            'gambarmasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/909673509507194910/diperas.jpg',
         ]);
 DB::table('masak')->insert([
             'idmasak'=>'10',
             'namamasak'=>'Dibekukan',
-            'gambamasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/915619505064009749/Dibekukan.jpg',
+            'gambarmasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/915619505064009749/Dibekukan.jpg',
         ]);
 DB::table('masak')->insert([
             'idmasak'=>'11',
             'namamasak'=>'Dipresto',
-            'gambamasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/918385443261808680/Dipresto.jpg',
+            'gambarmasak'=>'https://cdn.discordapp.com/attachments/906163180328325130/918385443261808680/Dipresto.jpg',
         ]);
     }
 }
